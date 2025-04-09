f651b9f2adf4a3b9ba1ff702cb36dab9c848b31d
Hannya Yan <yilei_yan@ucsb.edu>
2025-04-08 17:04:45 -0700

    HY - added test cases for equals() and hashCode()

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,4 +26,47 @@ public class TeamTest {
         assertEquals("Team(name=test-team, members=[])", team.toString());
     }
 
+    @Test
+    public void equals_same_object() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_different_class() {
+        assertFalse(team.equals("a string"));
+    }
+
+    @Test
+    public void equals_sameNameSameMembers() {
+        Team otherTeam = new Team("test-team");
+        assertTrue(team.equals(otherTeam));
+    }
+
+    @Test
+    public void equals_sameNameDiffMembers() {
+        Team otherTeam = new Team("test-team");
+        otherTeam.addMember("A");
+        assertFalse(team.equals(otherTeam));
+    }
+
+    @Test
+    public void equals_diffNameSameMembers() {
+        Team otherTeam = new Team("diff-name");
+        assertFalse(team.equals(otherTeam));
+    }
+
+    @Test
+    public void equals_diffNameDiffMembers() {
+        Team otherTeam = new Team("diff-name");
+        otherTeam.addMember("A");
+        assertFalse(team.equals(otherTeam));
+    }
+
+    @Test
+    public void hashCode_test() {
+        Team t = new Team("t");
+        int result = t.hashCode();
+        int expectedResult = 117;
+        assertEquals(expectedResult, result);
+    }
 }
